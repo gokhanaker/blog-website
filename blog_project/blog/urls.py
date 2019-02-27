219b61f334d65e0be575f6e83e19198d811591e9
Unknown <gokhanaker@gmail.com>
2019-02-27 13:49:26 +0100

    post detail functionality is added

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -6,4 +6,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.postList, name='postList'),
+    # path('about/', views.about, name='about'),
+    path('blog/<int:post_id>/', views.postDetail, name='postDetail'),
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -12,3 +12,15 @@ def postList(request):
     post_list = Post.objects.filter(publishedDate__lte = timezone.now()).order_by('-publishedDate')
     context = {'post_list': post_list}
     return render(request, 'blog/post_list.html', context)
+
+def postDetail(request, post_id):
+    # retrieving post object from database using its primary key
+    post = get_object_or_404(Post, pk = post_id)
+    # sending that post object as a parameter to template
+    return render (request, 'blog/post_detail.html', {'post': post})
+
+
+
+
+#def about(request):
+    #return render(request, 'blog/about.html')
